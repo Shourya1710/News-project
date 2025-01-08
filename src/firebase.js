34f110efc403a6c74a932a68666429c0d80bf261
Shourya Joshi <jshourya007@gmail.com>
2025-01-08 17:22:35 +0530

    removed api key

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,14 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCUVIsyJQg2gZXRq4gdHs2eEfnxF4B9RIg",
-  authDomain: "login-73322.firebaseapp.com",
-  projectId: "login-73322",
-  storageBucket: "login-73322.appspot.com", // Fixed typo
-  messagingSenderId: "363951766172",
-  appId: "1:363951766172:web:0f256189fcf9f828c24b59",
-  measurementId: "G-VFVJ2EDYRV",
+  apiKey: "djbfjds",
+  authDomain: "dsfds",
+  projectId: "ldsfd22",
+  storageBucket: "ldsf", // Fixed typo
+  messagingSenderId: "dsf",
+  appId: "1dsfb59",
+  measurementId: "G-dsfdssYRV",
 };
-
 
 // Initialize Firebase app
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
